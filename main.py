43b8d757dab4f0db580ad2e53c0e4d1889cdfbe7
MariamHamidi <mariam.hamidi@habuild.in>
2025-02-13 16:55:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,18 +20,20 @@ gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model
 
+Recipe_template = "Give me {number} recipes of {topic}"
+Recipe_prompt = PromptTemplate(template = Recipe_template, input_variables = ['number','topic'])
+recipe_chain = Recipe_prompt | gemini_model
 
-import streamlit as st
 
-st.header("Tweet Generator 🎁")
+st.header("Recipe Generator 🎁")
 
-st.subheader("Generate tweets using Generative AI")
+st.subheader("Generate recipes using Generative AI")
 
 topic = st.text_input("Topic")
 
-number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
+number = st.number_input("Number of recipes", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
+    tweets = recipe_chain.invoke({"number" : number, "topic" : topic})
     st.write(tweets.content)
     
