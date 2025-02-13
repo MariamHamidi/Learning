db6841015389d593d2b1d3ccf80f30e6c2645c29
MariamHamidi <mariam.hamidi@habuild.in>
2025-02-13 17:10:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model
 
-Recipe_template = "Give me {number} recipes of {topic}"
-Recipe_prompt = PromptTemplate(template = Recipe_template, input_variables = ['number','topic'])
+Recipe_template = "Give me {number} recipes of {topic} where {type} can be chosen"
+Recipe_prompt = PromptTemplate(template = Recipe_template, input_variables = ['number','topic','type'])
 recipe_chain = Recipe_prompt | gemini_model
 
 
@@ -29,13 +29,13 @@ st.header("Recipe Generator 🍔")
 
 st.subheader("Generate recipes using Generative AI")
 
-st.pills("Tags", ["Vegan", "Vegetarian", "Non-Vegetarian"])
-
 topic = st.text_input("Topic")
 
 number = st.number_input("Number of recipes", min_value = 1, max_value = 10, value = 1, step = 1)
 
+type = st.pills("Tags", ["Vegan", "Vegetarian", "Non-Vegetarian"])
+
 if st.button("Generate"):
-    recipes = recipe_chain.invoke({"number" : number, "topic" : topic})
+    recipes = recipe_chain.invoke({"number" : number, "topic" : topic, "type" : type})
     st.write(recipes.content)
     
